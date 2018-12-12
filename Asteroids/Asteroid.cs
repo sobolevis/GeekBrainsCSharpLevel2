e439c9e95362c0dac5ed1c58c8a828848859f781
SobolevIvan <sobolev.is.rus@gmail.com>
2018-12-12 14:23:26 +0700

    Complete two tasks

---
+++ Asteroids/Asteroid.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace Asteroids
+{
+    class Asteroid : BaseObject
+    {
+        protected static readonly Image ReferenceImage;
+
+        static Asteroid()
+        {
+            ReferenceImage = System.Drawing.Image.FromFile("Asteroid.png");
+        }
+
+        public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            Image = new Bitmap(ReferenceImage, size);
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(Image, Pos);
+        }
+
+        public override void Update() { }
+
+    }
+}
---
+++ Asteroids/Game.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Asteroids
+{
+    class Game
+    {
+        private static readonly Random Rnd = new Random();
+
+        private static BufferedGraphicsContext _context;
+
+        public static BaseObject[] Objs;
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game() {}
+
+        public static void Init(Form form)
+        {
+            Graphics g;
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Timer timer = new Timer { Interval = 20 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+            Load();
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Load()
+        {
+            Objs = new BaseObject[50];
+            for (int i = 0; i < Objs.Length; i++)
+            {
+                var rndObj = Rnd.Next(2);
+                var rndSize = Rnd.Next(5, 20);
+                if (rndObj == 1)
+                {
+                    Objs[i] = new Star(new Point(Rnd.Next(600), Rnd.Next(800)),
+                                        new Point(15 - i, 15 - i),
+                                        new Size(rndSize, rndSize));
+                }
+                else
+                {
+                    Objs[i] = new Asteroid(new Point(Rnd.Next(600), Rnd.Next(800)),
+                                            new Point(15 - i, 15 - i),
+                                            new Size(rndSize, rndSize));
+                }
+
+            }
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in Objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in Objs)
+                obj.Update();
+        }
+
+    }
+}
